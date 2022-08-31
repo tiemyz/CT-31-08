40385842f5a29ba3857c8f51c8780f55458004e6
tiemyz <rm95145@fiap.com.br>
2022-08-31 08:34:22 -0300

    commit.

---
+++ fatorial.py
@@ -0,0 +1,10 @@
+################## FATORIAL ##################
+#faça uma função que acha a fatorial de um número 
+#5! = 1*2*3*4*5
+
+n = 5
+fatorial = 1
+
+for i in range(1,n+1): 
+    fatorial = fatorial * i 
+print ("O fatorial de 5 é : ", fatorial) 
